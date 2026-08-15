33e8eb8355c1ae018a16b2e6fce6d193a96db6be
AlexJp223 <alexaderz514@gmail.com>
2026-08-15 14:19:29 -0500

    Programa de arreglo

---
+++ Arregloprograma.py
@@ -0,0 +1,13 @@
+# Tarea Semana 9 - Arreglo 
+# Nombre: Joel Alexander Zavala Pachay 
+
+# 1. Crear un arreglo con los valores solicitados
+numeros = [10, 20, 30, 40, 50]
+
+# 2. Mostrar el número almacenado en la posición 1
+print("El número en la posición 1 es:", numeros[1])
+
+# 3. Recorrer todo el arreglo desde la primera posición
+print("Todos los números del arreglo:")
+for num in numeros:
+    print(num)
